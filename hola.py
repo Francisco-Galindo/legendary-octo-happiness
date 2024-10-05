c02ad9d3debed6ff72387a99264d214cca349364
ksobrenat32 <ksobrenatural@tuta.io>
2024-10-04 19:03:20 -0600

    Mostrando la diferencia de tiempo entre iterativo y recursivo

--- hola.py
+++ hola.py
@@ -1,7 +1,11 @@
 # Este es un archivo de prueba
 
+tiempo = 0
+
 # Fibonnaci recursivo
 def rec_fib(n):
+    global tiempo
+    tiempo += 1
     if n == 0:
         return 0
     if n == 1:
@@ -9,6 +13,7 @@ def rec_fib(n):
     return rec_fib(n-1) + rec_fib(n-2)
 
 def ite_fib(n):
+    global tiempo
     if n == 0:
         return 0
     if n == 1:
@@ -16,9 +21,16 @@ def ite_fib(n):
     a = 0
     b = 1
     for i in range(2, n+1):
+        tiempo += 1
         c = a + b
         a = b
         b = c
     return b
 
-print(ite_fib(10))
+tiempo = 0
+print("Fibonacci recursivo de 40: ", rec_fib(40))
+print("Tiempo: ", tiempo)
+
+tiempo = 0
+print("Fibonacci iterativo de 40: ", ite_fib(40))
+print("Tiempo: ", tiempo)
