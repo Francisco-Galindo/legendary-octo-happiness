6beb389ce3496762deba8d05c8d7f7d36bd72521
ksobrenat32 <ksobrenatural@tuta.io>
2024-10-04 18:38:25 -0600

    Agregar el iterativo :))
    Solo dejando la más rápida, obvio la recursiva ;b

--- hola.py
+++ hola.py
@@ -8,4 +8,17 @@ def rec_fib(n):
         return 1
     return rec_fib(n-1) + rec_fib(n-2)
 
-print(rec_fib(10))
+def ite_fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a = 0
+    b = 1
+    for i in range(2, n+1):
+        c = a + b
+        a = b
+        b = c
+    return b
+
+print(ite_fib(10))
